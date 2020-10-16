a1f63445a5942b5e8ffec1bdb56a5956feef514f
sushma.k <sushma.k@metricstream.com>
2020-10-16 19:01:46 +0530

    Added ‘Home’ component

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
+import Home from './screens/Home/Home';
 
 ReactDOM.render(
-  <div></div>,
+  <Home></Home>,
   document.getElementById('root')
 );
 
